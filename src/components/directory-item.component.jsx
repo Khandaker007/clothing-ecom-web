2c80b2428c37cd8e933f701c00d67326b33b952d
ninad <na.ninad60@gmail.com>
2021-07-09 00:19:55 +0600

    update on styling

--- src/components/directory-item.component.jsx
+++ src/components/directory-item.component.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 import './directory-item.style.scss'
 
 const DirectoryItem = ({itemType, title, imageUrl}) => (
-    <div style={{
-            backgroundImage: `url(${imageUrl})`   
-        }} 
-        className={itemType}>
+    <div className={itemType}>
+        <div className="background-image" style={{backgroundImage:`url(${imageUrl}`}}/>
         <div className="content">
             <h1 className="content__title">{title}</h1>
             <span className="content__subtitle">shop now</span>
