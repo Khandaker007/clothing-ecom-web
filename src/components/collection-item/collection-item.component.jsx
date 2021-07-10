6e4e428a4afe971471e0789be6f1b33d8ee9942d
ninad <na.ninad60@gmail.com>
2021-07-11 01:31:17 +0600

    shop card style updated

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,7 +8,7 @@ const CollectionItem = ({name, price, imageUrl}) => (
         <button className="collection-item__btn">add to card</button>
         <div className="collection-item__footer">
             <span className='collection-item__name'>{ name }</span>
-            <span className="collection-item__price">{ price }</span>
+            <span className="collection-item__price">${ price }</span>
         </div>
     </div>
 )
